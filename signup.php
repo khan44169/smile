1662cb136342897d0f7d87109032a26834a6d157
ahadnaeemi <shaikhahad199@gmail.com>
2022-02-27 18:24:37 +0530

    first commit

---
+++ signup.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/sign-up.css">
+    
+    <title>SignUp Page</title>
+</head>
+
+<body>
+    <div class="login-box" id="signup-div">
+        <h2>Register yourself...</h2>
+        <form>
+            <div class="user">
+                <label>You Are!!!</label></br>
+                  <input type="radio" id="NGO" name="user" value="NGO" required>
+  <label for="NGO">NGO</label><br>
+  <input type="radio" id="HOTEL" name="user" value="HOTEL">
+  <label for="HOTEL">HOTEL</label><br>
+  <input type="radio" id="SINGLE" name="user" value="SINGLE">
+  <label for="SINGLE">SINGLE OWNER</label>
+            </div>
+            <div class="user-box">
+                <input type="text" name="" required>
+                <label>Hotel Name</label>
+            </div>
+            <div class="user-box">
+                <input type="text" name="" required="">
+                <label>License Number</label>
+            </div>
+            <div class="user-box">
+                <input type="text" name="" required="">
+                <label>E-mail</label>
+            </div>
+            <div class="user-box">
+                <input type="password" name="" required="">
+                <label>Password</label>
+            </div>
+            <div class="user-box">
+                <input type="password" name="" required="">
+                <label>Confirm Password</label>
+            </div>
+            <div class="user-box">
+                <input type="text" name="" required="">
+                <label>Address</label>
+            </div>
+            <a href="#" id="btn">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span> Submit
+            </a>
+        </form>
+    </div>
+
+  
+</body>
+
+</html>
