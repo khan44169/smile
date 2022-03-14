a68502180d8ae05c862fd50c5fa4497505a222be
khan Abu Talha <khan44169@gmail.com>
2022-03-14 20:32:23 +0530

    ABEverything up-to-date

--- index.php
+++ index.php
@@ -4,7 +4,44 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
+    <title>FoodTracker</title>
+    <link rel="stylesheet" href="./css/utility.css" />
+    <link rel="stylesheet" href="./css/index.css" />
   </head>
-  <body></body>
+
+  <body>
+    <!-- <section id="navbar-section"> -->
+    <!-- <nav class="navbar">
+      <div class="Name">FoodTracker</div>
+      <ul> -->
+    <!-- <li><a href="ngoProfile.html">NGO</a></li>
+        <li><a href="hotelProfile.html">Hotel</a></li>
+        <li><a href="login.html">Login</a></li>
+        <li><a href="signup.php">Signup</a></li> -->
+    <!-- </ul>
+    </nav> -->
+    <!-- </section> -->
+    <!-- <div class="logoSection">
+      <img src="ft2.png" alt="" />
+    </div> -->
+    <div id="ihavefood">
+      <button class="btn btn-hover">I have Food</button>
+    </div>
+
+    <!-- <div class="aboutSection"> -->
+    <!-- <img src="/pictures/ft2.png" alt=""> -->
+    <!-- <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+        facilis sapiente voluptatibus tempora, repellendus neque sunt ipsa
+        laboriosam pariatur. Quasi ullam eveniet veritatis quaerat, minus sunt
+        iusto veniam numquam molestiae illum, architecto nihil ab laboriosam
+        sed, dolorem nobis aliquid error alias voluptatum mollitia laudantium
+        magni quae. Ut dolorum veniam neque quia vero? Nesciunt temporibus
+        tenetur quidem ipsa error eaque eveniet mollitia praesentium impedit
+        amet.
+      </p>
+      <button class="btn btn-hover">Learn More</button>
+    </div> -->
+  </body>
 </html>
