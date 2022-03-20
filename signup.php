3882e49ad5b440c540c52ed6e16262530a48209d
ahadnaeemi <shaikhahad199@gmail.com>
2022-03-20 14:23:11 +0530

    signup page updated

--- signup.php
+++ signup.php
@@ -16,47 +16,71 @@
         <form>
             <div class="user">
                 <label>You Are!!!</label></br>
-                  <input type="radio" id="NGO" name="user" value="NGO" required>
+                  <input type="radio" id="NGO" name="user" value="NGO" onclick="show1();"required>
   <label for="NGO">NGO</label><br>
-  <input type="radio" id="HOTEL" name="user" value="HOTEL">
+  <input type="radio" id="HOTEL" name="user" value="HOTEL" onclick="show2();">
   <label for="HOTEL">HOTEL</label><br>
-  <input type="radio" id="SINGLE" name="user" value="SINGLE">
+  <input type="radio" id="SINGLE" name="user" value="SINGLE" onclick="show3();">
   <label for="SINGLE">SINGLE OWNER</label>
             </div>
-            <div class="user-box">
-                <input type="text" name="" required>
-                <label>Hotel Name</label>
+            <div id="NGO-signup" class="hide">
+                <div class="user-box">
+                    <input type="text" id="" name="" required>
+                    <label>NGO Name</label>
+                </div>
+                <div class="user-box">
+                    <input type="text" id="" name="" required>
+                    <label>Registeration Number</label>
+                </div>
+            </div>
+            <div id="HOTEL-signup"class="hide">
+                <div class="user-box">
+                    <input type="text" id="" name="" required>
+                    <label>Hotel Name</label>
+                </div>
+                <div class="user-box">
+                    <input type="text" id="" name="" required>
+                    <label>License Number</label>
+                </div>
             </div>
             <div class="user-box">
-                <input type="text" name="" required="">
-                <label>License Number</label>
-            </div>
-            <div class="user-box">
-                <input type="text" name="" required="">
+                <input type="text" id="" name="" required>
                 <label>E-mail</label>
             </div>
             <div class="user-box">
-                <input type="password" name="" required="">
+                <input type="password" id="" name="" required>
                 <label>Password</label>
             </div>
             <div class="user-box">
-                <input type="password" name="" required="">
+                <input type="password" id="" name="" required>
                 <label>Confirm Password</label>
             </div>
             <div class="user-box">
-                <input type="text" name="" required="">
+                <input type="text" id="" name="" required>
                 <label>Address</label>
             </div>
-            <a href="#" id="btn">
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span> Submit
-            </a>
+            <button type="submit" class="submit-button"> 
+                Submit
+            </button>
         </form>
     </div>
 
   
 </body>
+
+<script>
+    function show1(){
+  document.getElementById('NGO-signup').style.display ='block';
+  document.getElementById('HOTEL-signup').style.display ='none';
+}
+function show2(){
+  document.getElementById('NGO-signup').style.display = 'none';
+  document.getElementById('HOTEL-signup').style.display = 'block';
+}
+function show3(){
+  document.getElementById('NGO-signup').style.display = 'none';
+  document.getElementById('HOTEL-signup').style.display = 'none';
+}
+</script>
 
 </html>
