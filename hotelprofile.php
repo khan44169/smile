ce749e0340550bb3bda4efef766f383696e07bff
ahadnaeemi <shaikhahad199@gmail.com>
2022-03-26 17:39:07 +0530

    hotelprofile.php updated

--- hotelprofile.php
+++ hotelprofile.php
@@ -1,94 +1,56 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/HotelProfile.css">
+    <link rel="stylesheet" href="./css/hotelprofile.css">
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
-    <title>Hotel Profile</title>
+    <title>HOtel Profile</title>
 </head>
-
 <body>
-    <div class="slideshow-container">
+    <div id="triangle"></div>
+    <div id="page-container">
+        <div id="profile-container">
+            <div id="profile-picture-container">
+                <div id="profile-picture">
+                    <div id="image">
+                        <img src="./pictures/profile.png" alt="">
+                    </div>
+                    <div id="hotel-name-and-address">
+                        <p>
+                            <h2>
+                                Hotel Name
+                            </h2>
+                            address
+                            
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div id="hotel-info-and-socialmedia">
+                <h3> Information </h3>
+                <div class="row">
+                    <div class="col">
+                        <label for="Email">Email</label>
+                        <h6>Enetr email here</h6>
+                    </div>
+                    <div class="col">
+                        <label for="phone">Phone</label>
+                        <h6>Enetr phone here</h6>
+                    </div>
 
-        <div class="mySlides fade">
-            <div class="numbertext">1 / 3</div>
-            <img src="./pictures/download.jfif" style="width:100%">
-            <div class="text">Front view</div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <label for="smile served">Smile Served</label>
+                        <hr>
+                        1000 smile served
+                    </div>
+                </div>
+            </div>
+
         </div>
-
-        <div class="mySlides fade">
-            <div class="numbertext">2 / 3</div>
-            <img src="./pictures/maxresdefault.jpg" style="width:100%">
-            <div class="text">Room view</div>
-        </div>
-
-        <div class="mySlides fade">
-            <div class="numbertext">3 / 3</div>
-            <img src="./pictures/facebook.png" style="width:100%">
-            <div class="text">surrounding</div>
-        </div>
-
-        <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-        <a class="next" onclick="plusSlides(1)">&#10095;</a>
-
     </div>
-    <br>
-
-    <div style="text-align:center">
-        <span class="dot" onclick="currentSlide(1)"></span>
-        <span class="dot" onclick="currentSlide(2)"></span>
-        <span class="dot" onclick="currentSlide(3)"></span>
-    </div>
-
-    <div id=card>
-        <pre>
-            <h3>Taj Palace</h3>
-            Bhindibazar opposite sulabh shauchalya mumbai 01        
-
-    
-            contact No: 8828495484</br>
-            <a class="fab fa-facebook" href="../html/img.html"></a><a class="fab fa-instagram"href="../html/img.html"></a><a class="fab fa-whatsapp"href="../html/img.html"></a>
-        </pre>
-
-
-
-    </div>
-
 </body>
-<script>
-    var slideIndex = 1;
-    showSlides(slideIndex);
-
-    function plusSlides(n) {
-        showSlides(slideIndex += n);
-    }
-
-    function currentSlide(n) {
-        showSlides(slideIndex = n);
-    }
-
-    function showSlides(n) {
-        var i;
-        var slides = document.getElementsByClassName("mySlides");
-        var dots = document.getElementsByClassName("dot");
-        if (n > slides.length) {
-            slideIndex = 1
-        }
-        if (n < 1) {
-            slideIndex = slides.length
-        }
-        for (i = 0; i < slides.length; i++) {
-            slides[i].style.display = "none";
-        }
-        for (i = 0; i < dots.length; i++) {
-            dots[i].className = dots[i].className.replace(" active", "");
-        }
-        slides[slideIndex - 1].style.display = "block";
-        dots[slideIndex - 1].className += " active";
-    }
-</script>
-
 </html>
