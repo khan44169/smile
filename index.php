98349f4a95570a4c8b3d7dbe63f24df6017e2c7c
khan Abu Talha <khan44169@gmail.com>
2022-03-27 23:03:43 +0530

    index page updated

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ if (isset($_SESSION['ngo_login']) || isset($_SESSION['hotel_login']) || isset($_
 <body>
   <?php include './nav.php'; ?>
   <div id="triangle"></div>
- 
+
   <div id="ihavefood">
     <form action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" method="post">
       <button class=" btn-hover" name="ihavefoodbtn">I have Food</button>
@@ -39,6 +39,20 @@ if (isset($_SESSION['ngo_login']) || isset($_SESSION['hotel_login']) || isset($_
     }
     ?>
 
+  </div>
+  <div id="footer">
+    <div id="foodraised">
+      <div id="smileserved-container">
+        <div id="weserved">
+          <h3>we served</h3>
+        </div>
+        <div id="smileserved">8500</div>
+        <div id="smiles"> 
+          <h4>smiles</h4>
+        </div>
+      </div>
+    </div>
+    <div id="aboutus"></div>
   </div>
 
 </body>
